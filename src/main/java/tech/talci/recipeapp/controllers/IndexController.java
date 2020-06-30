baf319903f4b5d59e31fe04bdf319f5db63fdb8e
akmal2409 <akmalalikhujaev@gmail.com>
2020-06-30 15:31:05 +0200

    Added Controller

---
+++ src/main/java/tech/talci/recipeapp/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package tech.talci.recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","/index","/index.html"})
+    public String getIndexPage(){
+        System.out.println("Some message to say... Kur3211ac!");
+        return "index";
+    }
+}
