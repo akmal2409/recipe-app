3d6fc70473383471c9c88e3c2c85222d85286e3e
akmal2409 <akmalalikhujaev@gmail.com>
2020-07-10 15:43:27 +0200

    Added Integration Test for UnitOfMeasure Repository, added Maven Failsafe plugin for Integration Tests

--- src/test/java/tech/talci/recipeapp/repositories/UnitOFMeasureRepositoryIT.java
+++ src/test/java/tech/talci/recipeapp/repositories/UnitOFMeasureRepositoryIT.java
@@ -2,10 +2,23 @@ package tech.talci.recipeapp.repositories;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import tech.talci.recipeapp.domain.UnitOfMeasure;
+
+import java.util.Optional;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class UnitOFMeasureRepositoryIT {
+
+    @Autowired
+    UnitOFMeasureRepository unitOFMeasureRepository;
 
     @Before
     public void setUp() throws Exception {
@@ -13,5 +26,17 @@ public class UnitOFMeasureRepositoryIT {
 
     @Test
     public void findByDescription() {
+
+        Optional<UnitOfMeasure> optional = unitOFMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", optional.get().getDescription());
+    }
+
+    @Test
+    public void findByDescriptionCup() {
+
+        Optional<UnitOfMeasure> optional = unitOFMeasureRepository.findByDescription("Cup");
+
+        assertEquals("Cup", optional.get().getDescription());
     }
 }
