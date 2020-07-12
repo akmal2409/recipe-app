b107facc895705f9ab0bb4dd5eec1f9bf27e97cf
akmal2409 <akmalalikhujaev@gmail.com>
2020-07-12 16:51:40 +0200

    Added simple POJO's for Commands

--- src/main/java/tech/talci/recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/tech/talci/recipeapp/bootstrap/RecipeBootstrap.java
@@ -110,6 +110,9 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         guacRecipe.setDescription("Perfect Guacamole");
         guacRecipe.setPrepTime(10);
         guacRecipe.setCookTime(8);
+        guacRecipe.setSource("My Grandma");
+        guacRecipe.setUrl("www.recipe-god.com");
+        guacRecipe.setServings(3);
         guacRecipe.setDifficulty(Difficulty.EASY);
         guacRecipe.setDirections("1 Cut avocado, remove flesh: Cut the avocados in half. Remove seed. Score the inside of the avocado with a blunt knife and scoop out the flesh with a spoon" +
                 "\n" +
@@ -150,6 +153,9 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         tacosRecipe.setCookTime(9);
         tacosRecipe.setPrepTime(20);
         tacosRecipe.setDifficulty(Difficulty.MODERATE);
+        tacosRecipe.setSource("My Grandma");
+        tacosRecipe.setUrl("www.recipe-god.com");
+        tacosRecipe.setServings(3);
 
         tacosRecipe.setDirections("1 Prepare a gas or charcoal grill for medium-high, direct heat.\n" +
                 "2 Make the marinade and coat the chicken: In a large bowl, stir together the chili powder, oregano, cumin, sugar, salt, garlic and orange zest. Stir in the orange juice and olive oil to make a loose paste. Add the chicken to the bowl and toss to coat all over.\n" +
---
+++ src/main/java/tech/talci/recipeapp/commands/RecipeCommand.java
@@ -0,0 +1,29 @@
+package tech.talci.recipeapp.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import tech.talci.recipeapp.domain.Category;
+import tech.talci.recipeapp.domain.Difficulty;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RecipeCommand {
+    private Long id;
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    private Set<IngredientCommand> ingrediends = new HashSet<>();
+    private Difficulty difficulty;
+    private NotesCommand notes;
+    private Set<CategoryCommand> categories = new HashSet<>();
+}
